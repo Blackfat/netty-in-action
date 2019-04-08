e94dcf33a5a4a4c579ba273dbfbac57fd109d4e4
Blackfat <15715556946@163.com>
2019-04-09 06:59:10 +0800

    Recycle

---
+++ netty-telnet/src/test/java/com/blackfat/netty/RecycleTest.java
@@ -0,0 +1,41 @@
+package com.blackfat.netty;
+
+import io.netty.util.Recycler;
+
+/**
+ * @author blackfat
+ * @create 2019-04-09-上午6:37
+ */
+public class RecycleTest {
+
+    private static final Recycler<User> RECYCLER = new Recycler<User>() {
+        @Override
+        protected User newObject(Handle<User> handle) {
+            return new User(handle);
+        }
+    };
+
+    private static class User{
+        private final Recycler.Handle<User> handle;
+
+        public User(Recycler.Handle<User> handle){
+            this.handle = handle;
+        }
+
+        public void recycle(){
+            handle.recycle(this);
+        }
+    }
+
+    public static void main(String[] args) {
+        User user = RECYCLER.get();
+
+        user.recycle();
+
+        User user1 = RECYCLER.get();
+
+        System.out.println(user == user1);
+    }
+
+
+}
